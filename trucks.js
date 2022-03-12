aa9ce27ef63f4c25392f6ae598bede0282c8491e
jwalla7 <j7wallace7@outlook.com>
2022-03-12 15:04:17 -0600

    mar 12 0304

--- trucks.js
+++ trucks.js
@@ -1,22 +1,104 @@
-mackTerraPro = {
-    "year": 2022,
-    "make": "Mack",
-    "model": "TerraPro",
-    "engines": ["MP7", "MP8", "Cummins Wesport L9N Natural Gas"],
-    "transmissions": ["Allison 3000 Series", "Allison 4500 Series"],
-    "isRunning": true,
-    "builds": [
-        "Tractor",
-        "Tank",
-        "Vacuum",
-        "Concrete",
-        "Flat-Bed",
-        "Sand",
-        "Front Loader Refuse",
-        "Rear Loader Refuse",
-        "Specialty Applications"
-    ],
-    "class": 8,
-    "horsepower": 505,
-    "startingPrice": 280000.00
-}
+    mackTerraPro = {
+        "year": 2022,
+        "make": "Mack",
+        "model": "TerraPro",
+        "engines": ["MP7", "MP8", "Cummins Westport L9N Natural Gas"],
+        "transmissions": ["Allison 3000 Series", "Allison 4500 Series"],
+        "isRunning": true,
+        "builds": [
+            "Tractor",
+            "Tank",
+            "Vacuum",
+            "Concrete",
+            "Flat-Bed",
+            "Sand",
+            "Front Loader Refuse",
+            "Rear Loader Refuse",
+            "Specialty Applications"
+        ],
+        "class": 8,
+        "horsepower": 505,
+        "startingPrice": 280000.00
+    }
+    mackLR = {
+        "year": 2023,
+        "make": "Mack",
+        "model": "LR",
+        "engines": ["MP7", "MP8", "Cummins Westport L9N Natural Gas"],
+        "transmissions": ["Allison 3000 Series", "Allison 4500 Series"],
+        "isRunning": true,
+        "builds": [
+            "Tank",
+            "Flat-Bed",
+            "Side Loader Refuse",
+            "Front Loader Refuse",
+            "Rear Loader Refuse",
+            "Specialty Applications"
+        ],
+        "class": 8,
+        "horsepower": 355,
+        "startingPrice": 269000.00
+    }
+    peterbilt520 = {
+        "year": 2023,
+        "make": "Peterbilt",
+        "model": 520,
+        "engines": ["Paccar MX", "Paccar PX", "Cummins Westport L9N Natural Gas"],
+        "transmissions": ["Eaton","Allison 3000 Series", "Allison 4500 Series"],
+        "isRunning": true,
+        "builds": [
+            "Tank",
+            "Flat-Bed",
+            "Sand",
+            "Front Loader Refuse",
+            "Rear Loader Refuse",
+            "Specialty Applications"
+        ],
+        "class": 8,
+        "horsepower": 385,
+        "startingPrice": 208000.00
+    }
+    peterbilt389 = {
+        "year": 2022,
+        "make": "Peterbilt",
+        "model": 389,
+        "engines": ["Cummins X15","Paccar MX", "Paccar PX", "Cummins Westport L9N Natural Gas"],
+        "transmissions": ["Fuller","Eaton","Allison 3000 Series", "Allison 4500 Series"],
+        "isRunning": true,
+        "builds": [
+            "Wrecker",
+            "Tractor",
+            "Tank",
+            "Vacuum",
+            "Concrete",
+            "Flat-Bed",
+            "Sand",
+            "Front Loader Refuse",
+            "Rear Loader Refuse",
+            "Specialty Applications"
+        ],
+        "class": 8,
+        "horsepower": 605,
+        "startingPrice": 550000.00
+    }
+    autocarXpeditor = {
+        "year": 2022,
+        "make": "Autocar",
+        "model": "ACX 64",
+        "engines": ["Cummins X12", "Cummins Westport L9N Natural Gas"],
+        "transmissions": "Allison 4500 Series",
+        "isRunning": true,
+        "builds": [
+            "Tank",
+            "Vacuum",
+            "Concrete",
+            "Flat-Bed",
+            "Sand",
+            "Front Loader Refuse",
+            "Rear Loader Refuse",
+            "Specialty Applications"
+        ],
+        "class": 8,
+        "horsepower": 350,
+        "startingPrice": 188000.00
+    }
