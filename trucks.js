5ae666bab6243c169fee15d7d3c9b5c29ff2cd84
jwalla7 <j7wallace7@outlook.com>
2022-03-12 14:01:25 -0600

    mar 12 0201

---
+++ trucks.js
@@ -0,0 +1,22 @@
+mackTerraPro= {
+    "year": 2022,
+    "make": "Mack",
+    "model": "TerraPro",
+    "engines": ["MP7", "MP8", "Cummins Wesport L9N Natural Gas"],
+    "transmissions": ["Allison 3000 Series", "Allison 4500 Series"],
+    "isRunning": true,
+    "builds": [
+        "Tractor",
+        "Tank",
+        "Vacuum",
+        "Concrete",
+        "Flat-Bed",
+        "Sand",
+        "Front Loader Refuse",
+        "Rear Loader Refuse",
+        "Specialty Applications"
+    ],
+    "class": 8,
+    "horsepower": 505,
+    "startingPrice": 280000.00
+}
